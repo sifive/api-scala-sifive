ed08cadde363f60147c9678d72e770c7215e2686
Jack Koenig <koenig@sifive.com>
2019-02-24 00:57:55 -0800

    WIP

--- getMill.sh
+++ getMill.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-DIR=$ROOT/bin
-VERSION=$1
+DIR=$1
+VERSION=$2
 REMOTE=https://github.com/lihaoyi/mill/releases/download/$VERSION/$VERSION
 BIN=$DIR/mill-$VERSION
 
-mkdir -p $DIR
 echo "#!/usr/bin/env sh" >> $BIN
 curl -sL $REMOTE >> $BIN
 chmod +x $BIN
