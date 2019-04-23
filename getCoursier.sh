708809e02aa440d6ac90969fdad80636b460fd08
Jack Koenig <koenig@sifive.com>
2019-04-22 18:41:24 -0700

    Remove vestigial mill and coursier fetching code

--- getCoursier.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$1
-VERSION=$2
-REMOTE=https://github.com/coursier/coursier/releases/download/v$VERSION/coursier
-BIN=$DIR/coursier-$VERSION
-
-curl -Lo $BIN $REMOTE
-chmod +x $BIN
